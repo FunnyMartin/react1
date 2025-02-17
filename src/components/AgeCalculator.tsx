206cce098555a5a5170008e74bfc2cb7bbfc9fbf
Martin Šilar <silar@SPSEJECNANET.CZ>
2025-02-17 13:58:51 +0100

    change

--- src/components/AgeCalculator.tsx
+++ src/components/AgeCalculator.tsx
@@ -4,13 +4,13 @@ import calcAge from '../utils';
 export default function AgeCalculator() {
     const [birthYear, setBirthYear] = useState(0);
     const [age, setAge] = useState(0);
-    const handleChange = (e) => {
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         const value = e.target.value;
-        setBirthYear(value);
-        if (value && !isNaN(value)) {
+        setBirthYear(Number(value));
+        if (value && !isNaN(Number(value))) {
             setAge(calcAge(parseInt(value, 10)));
         } else {
-            setAge(null);
+            setAge(0);
         }
     };
 
